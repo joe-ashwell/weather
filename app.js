cb54116d053942e6b52cbaea6a6600fad8e9e71d
joe-ashwell <67185069+joe-ashwell@users.noreply.github.com>
2020-11-10 20:02:30 +0000

    Add basic page template & start to include displaying results to page

--- app.js
+++ app.js
@@ -5,30 +5,70 @@
 //! - Let user search for a location
 //! - Serve weather info to user on page
 
-const endpoint = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=bc23f6fca36dd2062739984568b51cfa";
+let weatherEndpoint;
 const weather = [];
+const currentWeather = [];
+const hourlyWeather = [];
+const dailyWeather = [];
+
 
 const cityEndpoint = 'https://geolocation-db.com/json/';
 const positionArray = [];
 
-const resultCity = document.querySelector('h1.weather-result');
-const resultCountry = document.querySelector('h2.weather-result');
+const resultState = document.querySelector('h1.weather-result-hero');
+const resultCountry = document.querySelector('h2.weather-result-hero');
+const resultDate = document.querySelector('p.weather-result-hero');
 
 
-fetch(endpoint)
-  .then(blob => blob.json())
-  .then(data => weather.push(data))
-  .then(console.log(weather));
+const kelvinToCelcius = -273.15;
 
-fetch(cityEndpoint)
-  .then(blob => blob.json())
-  .then(data => positionArray.push(data))
-  .then(displayLocationResults);
+// Function to get the endpoint of users location
+function getLocationEndpoint() {
 
-console.log(positionArray);
+  fetch(cityEndpoint)
+    .then(blob => blob.json())
+    .then(data => positionArray.push(data))
+    .then(displayLocationResults)
+    .then(getLocalWeather);
+  
+}
+
+// function to get the weather info based on the users location
+function getLocalWeather() {
+
+  weatherEndpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${positionArray[positionArray.length-1].latitude}&lon=${positionArray[positionArray.length-1].longitude}&exclude=minutely&appid=bc23f6fca36dd2062739984568b51cfa`;
+
+  fetch(weatherEndpoint)
+    .then(blob => blob.json())
+    .then(data => {
+      weather.push(data)
+      currentWeather.push(data.current)
+      dailyWeather.push(data.daily)
+      hourlyWeather.push(data.hourly)
+    })
+    .then(console.log(weather))
+    .then(console.log(currentWeather))
+    .then(console.log(dailyWeather))
+    .then(console.log(hourlyWeather));
+
+
+}
 
 // Tried passing in the positionArray as an arguement, but that created a console error as when the script is ran initially the array is empty, so everything is undefined.
 function displayLocationResults() {
-  resultCity.innerHTML = `${positionArray[positionArray.length - 1].city}`;
+
+  resultState.innerHTML = `${positionArray[positionArray.length - 1].state}`;
   resultCountry.innerHTML = `${positionArray[positionArray.length - 1].country_name}`;
+  resultDate.innerHTML = convertTimestampToDate(1604911709101);
+
 }
+
+function convertTimestampToDate(timestamp) {
+
+  let date = new Date(timestamp);
+  return `${date.getDate() + 1}/${date.getMonth() + 1}/${date.getFullYear()}`;
+
+}
+
+// Invoke getLocationEndpoint on load
+window.addEventListener('load', getLocationEndpoint);
