56207b215787acb4c6f6986fba3b23322a06e02e
joe-ashwell <67185069+joe-ashwell@users.noreply.github.com>
2020-11-13 08:24:08 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 //? To do list
 //* - Get users locations deets
 //* - Translate location deets to city/region level (need API?)
-//! - Fetch weather deets based on users location (create a function to input the location using a template literal (same as ISS project))
-//! - Let user search for a location
+//* - Fetch weather deets based on users location (create a function to input the location using a template literal (same as ISS project))
 //! - Serve weather info to user on page
 
 let weatherEndpoint;
 const weatherArray = [];
-const currentWeather = [];
-const hourlyWeather = [];
-const dailyWeather = [];
-
 
 const cityEndpoint = 'https://geolocation-db.com/json/';
-const positionArray = [];
+const positionObj = {};
 
 const resultState = document.querySelector('h1.weather-result-hero');
 const resultCountry = document.querySelector('h2.weather-result-hero');
@@ -22,44 +17,52 @@ const resultDate = document.querySelector('p.weather-result-hero');
 const currentWeatherDescription = document.querySelector('p.middle-hero-weather');
 const currentTemp = document.querySelector('p.middle-hero-temp');
 
-// Function to get the endpoint of users location
-function getLocationEndpoint() {
+const getLocationPromise = new Promise(() => {
 
-  fetch(cityEndpoint)
-    .then(blob => blob.json())
-    .then(data => positionArray.push(data))
-    .then(getLocalWeather)
-    .then(displayHero);
-  
-}
+  navigator.geolocation.getCurrentPosition(function(position) {
 
-// function to get the weather info based on the users location
-function getLocalWeather() {
+    positionObj.latitude = position.coords.latitude;
+    positionObj.longitude = position.coords.longitude;
+    console.log(positionObj);
 
-  weatherEndpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${positionArray[positionArray.length-1].latitude}&lon=${positionArray[positionArray.length-1].longitude}&exclude=minutely&appid=bc23f6fca36dd2062739984568b51cfa`;
+  })
+
+});
+
+getLocationPromise.then(getLocalWeather = () => {
+
+  weatherEndpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${positionObj.latitude}&lon=${positionObj.longitude}&exclude=minutely&appid=bc23f6fca36dd2062739984568b51cfa`;
 
   fetch(weatherEndpoint)
     .then(blob => blob.json())
-    .then(data => {
-      weatherArray.push(data)
-      currentWeather.push(data.current)
-      dailyWeather.push(data.daily)
-      hourlyWeather.push(data.hourly)
-    })
+    .then(data => weatherArray.push(data))
     .then(console.log(weatherArray))
-    .then(console.log(currentWeather))
-    .then(console.log(dailyWeather))
-    .then(console.log(hourlyWeather))
     .then(displayHero);
+  
+    console.log(positionObj);
 
+})
 
-}
+// function to get the weather info based on the users location
+// function getLocalWeather() {
+
+//   weatherEndpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${positionObj.latitude}&lon=${positionObj.longitude}&exclude=minutely&appid=bc23f6fca36dd2062739984568b51cfa`;
+
+//   fetch(weatherEndpoint)
+//     .then(blob => blob.json())
+//     .then(data => weatherArray.push(data))
+//     .then(console.log(weatherArray))
+//     .then(displayHero);
+  
+//     console.log(positionObj);
+
+// }
 
 // Tried passing in the positionArray as an arguement, but that created a console error as when the script is ran initially the array is empty, so everything is undefined.
-function displayHero() {
+const displayHero = () => {
 
-  resultState.innerHTML = `${positionArray[positionArray.length - 1].state}`;
-  resultCountry.innerHTML = `${positionArray[positionArray.length - 1].country_name}`;
+  resultState.innerHTML = `${positionObj.latitude}`;
+  resultCountry.innerHTML = `${positionObj.longitude}`;
   resultDate.innerHTML = convertTimestampToDate(1604911709101);
 
   currentWeatherDescription.innerHTML = `${weatherArray[0].current.weather[0].description}`;
@@ -67,6 +70,7 @@ function displayHero() {
 
 }
 
+// Convert the Epoch timestamp to usable/readable date format
 function convertTimestampToDate(timestamp) {
 
   let date = new Date(timestamp);
@@ -74,11 +78,12 @@ function convertTimestampToDate(timestamp) {
 
 }
 
+// Convert valuse from kelvin to deg c
 function convertKelvintoDegC(kelvin) {
 
   return -273.15 + kelvin;
 
 }
 
-// Invoke getLocationEndpoint on load
-window.addEventListener('load', getLocationEndpoint);
+// Invoke getLocation on load
+window.addEventListener('load', getLocationPromise);
